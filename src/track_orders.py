cd297353956d23f534844c329aab4f18b79a8c82
lucas_almeida <lucas.almdiniz@gmail.com>
2022-12-14 17:41:16 -0300

    feat: Requisito 2: Análises contínuas
    método "get_most_ordered_dish_per_customer" implementado

--- src/track_orders.py
+++ src/track_orders.py
@@ -7,13 +7,31 @@ class TrackOrders:
 
     def add_new_order(self, customer, order, day):
         self.__orders_list.append({
-            "customer": customer,
-            "order": order,
-            "day": day,
+            "cliente": customer,
+            "pedido": order,
+            "dia": day,
         })
 
     def get_most_ordered_dish_per_customer(self, customer):
-        pass
+        dishes = dict()
+        orders_list = self.__orders_list
+        dish_most_ordered = orders_list[0]['pedido']
+        amount_dish_most_ordered = 0
+
+        for order in orders_list:
+            if order['cliente'] == customer:
+                pedido = order['pedido']
+
+                if pedido not in dishes:
+                    dishes[pedido] = 1
+                else:
+                    dishes[pedido] += 1
+
+                if dishes[pedido] > amount_dish_most_ordered:
+                    dish_most_ordered = pedido
+                    amount_dish_most_ordered = dishes[pedido]
+
+        return dish_most_ordered
 
     def get_never_ordered_per_customer(self, customer):
         pass
